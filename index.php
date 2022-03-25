002d87f536c72d847aadd1b2cc6a6ffdff7277fc
Nisarg <nisarg.phpdeveloper@gmail.com>
2022-03-25 13:36:46 +1300

    demo

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <h1>Git Demo</h1>
 <hr/>
-<?php if ($_SERVER['REQUEST_URI'] == '/git-development/') { ?>
+<?php if ($_SERVER['REQUEST_URI'] == '/git-development/' || $_SERVER['REQUEST_URI'] == '/learngit/') { ?>
     <h2>This is development</h2>
 <?php } ?>
 <?php if ($_SERVER['REQUEST_URI'] == '/git-production/') { ?>
