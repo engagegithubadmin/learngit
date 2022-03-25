cd2acd2f6d826bd9641356fa1f67a4106d074aff
CAndersonEngage <craig.anderson@engagesolutions.co.nz>
2022-03-25 13:41:33 +1300

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <hr/>
 <?php if ($_SERVER['REQUEST_URI'] == '/git-development/' || $_SERVER['REQUEST_URI'] == '/learngit/') { ?>
     <h2>This is development</h2>
+    <hr>Hello :)</hr>
 <?php } ?>
 <?php if ($_SERVER['REQUEST_URI'] == '/git-production/') { ?>
     <h2>This is production</h2>
