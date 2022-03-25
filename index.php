18384ab8b88f3b4d56f94af3d3512c7343e846ac
Nisarg <nisarg.phpdeveloper@gmail.com>
2022-03-25 13:06:18 +1300

    index.php file

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1>Git Demo</h1>
+<hr/>
+<?php if ($_SERVER['REQUEST_URI'] == '/git-development/') { ?>
+    <h2>This is development</h2>
+<?php } ?>
+<?php if ($_SERVER['REQUEST_URI'] == '/git-production/') { ?>
+    <h2>This is production</h2>
+<?php } ?>
